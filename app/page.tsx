26518927804d11db870e115676c98788c925f545
Sudhansh <96883913+SadhuG@users.noreply.github.com>
2025-09-29 00:53:58 +0530

    Added prettier class order, fixed hero carousal

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
-import Hero from '@/components/section/Hero'
-import Message from '@/components/section/Message'
-import Navbar from '@/components/section/Navbar'
-import Jewelry from '@/components/section/Jewelry'
-import Contact from '@/components/section/Contact'
-import React from 'react'
-import Footer from '@/components/section/Footer'
+import Contact from "@/components/section/Contact";
+import Footer from "@/components/section/Footer";
+import Hero from "@/components/section/Hero";
+import Jewelry from "@/components/section/Jewelry";
+import Message from "@/components/section/Message";
+import Navbar from "@/components/section/Navbar";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div className=' min-h-screen w-full bg-[#FFFFFF]'>
-      <Navbar/>
-      <Hero/>
-      <Message/>
-      <Jewelry/>
-      <Contact/>
-      <Footer/>
-    </div>
-  )
-}
+	return (
+		<div className=" min-h-screen w-full bg-background text-foreground">
+			<Navbar />
+			<Hero />
+			<Message />
+			<Jewelry />
+			<Contact />
+			<Footer />
+		</div>
+	);
+};
 
-export default Home
+export default Home;
